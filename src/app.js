d1b76d60f4db7cc3985b65be42a320fe9021ec7c
a9irudhh <01fe22bcs264@kletech.ac.in>
2025-03-13 12:46:17 +0530

    file upload middleware added

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors()) // app.use is a middleware function that adds middleware to the request handling pipeline
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+})) // app.use is a middleware function that adds middleware to the request handling pipeline
 app.use(express.json({limit: "16kb"})) // express.json() is a middleware function that parses incoming requests with JSON payloads
 app.use(express.urlencoded({extended: true})) // express.urlencoded() is a middleware function that parses incoming requests with urlencoded payloads
 app.use(express.static("public")) // express.static() is a middleware function that serves static files

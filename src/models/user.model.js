f802a0924d6cbf8b630dc42ea4229dd351632d22
a9irudhh <01fe22bcs264@kletech.ac.in>
2025-03-13 10:37:57 +0530

    added user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,4 +58,28 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateToken = function() {
+    return jwt.sign(
+    {
+        _id : this._id,
+        userName : this.userName,
+        email : this.email,
+        fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
+}
 export const User = mongoose.model('User', userSchema)
